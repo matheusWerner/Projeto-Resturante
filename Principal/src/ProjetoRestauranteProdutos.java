a4910abdda863cbe3acc4dcecdc00de26695e26d
gabriel Boeing <gabriel-dms@hotmail.com>
2018-05-22 10:25:26 -0300

    continuação

--- Principal/src/ProjetoRestauranteProdutos.java
+++ Principal/src/ProjetoRestauranteProdutos.java
@@ -13,10 +13,10 @@ import javax.swing.JOptionPane;
  */
 public class ProjetoRestauranteProdutos {
     
-    double transportadoraA[] = new double[5];
-    double transportadoraB[] = new double[5];
-    double transportadoraC[] = new double[5];
-    double transportadoraD[] = new double[5];
+    String transportadoras[] = new String[5];
+    String medidas[] = new String[5];
+    String dia[] = new String[5];
+    
     int atual = 0;
     
     public void cadastroProdutos(){
@@ -25,17 +25,64 @@ public class ProjetoRestauranteProdutos {
      
        }
     
-    public void solicitarInformação(int posicao){
-        transportadoraA[posicao] = Double.parseDouble(JOptionPane.showInputDialog("Digite Kg de proteinas"));
-        transportadoraB[posicao] = Double.parseDouble(JOptionPane.showInputDialog("Digite Kg de arroz, feijão, massa e grãos"));
-        transportadoraC[posicao] = Double.parseDouble(JOptionPane.showInputDialog("Digite Litros de bebidas em gerais"));
-        transportadoraD[posicao] = Double.parseDouble(JOptionPane.showInputDialog("Digite Kg de verduras e frutas"));
+    public void editarProdutos(){
+        String busca = JOptionPane.showInputDialog("Solicite o que deseja editar");
+        for(int i = 0; i < atual; i++){
+            if(transportadoras[i].equals(busca)){
+                solicitarInformação(i);
+                
+                return;
+                
+            }
+        }
+       
+    }
+    
+    public void listarProdutos(){
+        String texto = "";
+        for(int i = 0; i < atual; i++){
+        texto += transportadoras[i] + "  " + medidas[i] + "   " + dia[i] + "\n";
+    }
+        JOptionPane.showMessageDialog(null, texto);
+    }
+    
+    public void estatisticasProdutos(){
         
+    }
+    
+    public void buscarProdutos(){
+         String busca = JOptionPane.showInputDialog("Digite o nome parcial da transportadora para a buscar mais informações");
+         
+         for(int i = 0; i < atual; i++){
+             if(transportadoras[i].contains(busca)){
+                 solicitarInformação(i);
+                 
+             }
+         }
+    }
+    
+    public void solicitarInformação(int posicao){
+        transportadoras[posicao] = JOptionPane.showInputDialog("Digite qual transportadora");
+        medidas[posicao] = JOptionPane.showInputDialog("Digite quantidades do fornecimento em kg ou litros");
+        dia[posicao] = JOptionPane.showInputDialog("Dia do fornecimento").replace(".","").replace("-","").replace("/","");
+    }
+    
+    public void apresentarInformação(int i){
+        JOptionPane.showMessageDialog(null, 
+                "Nome da Transportadora: " + transportadoras[i] +
+                        "Carga fornecida: " + medidas[i] +
+                        "Dia do fornecimento: " + dia[i]
+                        
+                        
+                      
+        );
     }
 
     private void solicitarInformacao(int atual) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    } 
+    
+    
         
             
         
